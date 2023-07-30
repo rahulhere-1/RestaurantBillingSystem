97d5e41528f47950f11d4c96ed369ef0d8404200
rahulhere-1 <rmendigeri4@gmail.com>
2023-07-30 14:21:27 +0530

    initializing project

---
+++ backend/restaurant-billing-system/src/test/java/com/restaurant/restaurantbillingsystem/RestaurantBillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.restaurantbillingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
