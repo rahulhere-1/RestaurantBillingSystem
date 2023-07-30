dffdb171b4214d19a961625f164e2d7468498628
rahulhere-1 <rmendigeri4@gmail.com>
2023-07-30 22:31:53 +0530

    added jwt authn

---
+++ backend/restaurant-billing-system/src/main/java/com/restaurant/restaurantbillingsystem/JWT/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.restaurant.restaurantbillingsystem.JWT;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig implements WebSecurityConfigurer {
+    @Override
+    public void init(SecurityBuilder builder) throws Exception {
+
+    }
+
+    @Override
+    public void configure(SecurityBuilder builder) throws Exception {
+            builder
+    }
+}
